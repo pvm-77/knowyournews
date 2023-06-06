de8fde0b0295af51b258bdecefda7085cdc87b18
pvm-77 <sfzalig@gmail.com>
2023-06-06 11:44:47 +0530

    news card fixed

--- src/api/news.js
+++ src/api/news.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-
-
-const category = 'general';
-// const url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + apikey;
-const url = ' https://gnews.io/api/v4/top-headlines?category=general&lang=hi&apikey=ec8a1df338cca376771ebd8085a4494f'
-const getData = () => {
-    const response = axios.post(url)
+const fetchData = async (category) => {
+    const url = `https://gnews.io/api/v4/top-headlines?category=${category}&lang=hi&apikey=${process.env.REACT_APP_API_KEY}`
+    const response = await axios.get(url);
+    // console.log('data from api is',response);
+    return response.data
 }
+const newsService={fetchData}
+
+export default newsService;
--- src/components/news/NewsArticle.js
+++ src/components/news/NewsArticle.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import './news.css';
-// const NewsArticle = ({ article }) => {
-//     if (!article) {
-//         // If article data is missing or undefined, display an error message or fallback UI
-//         return <div className="error-message">Error: Unable to load article.</div>;
-//     }
-// 
-//     const { title, content, source, description, author, publishedAt, url, image } = article;
-// 
-//     return (
-//         <div className="news-article">
-//             {image && (
-//                 <img src={image} alt={title} className="article-image" />
-//             )}
-// 
-//             <h2 className="article-title">{title}</h2>
-//             {author && <p className="article-author">By {author}</p>}
-//             {publishedAt && (
-//                 <p className="article-date">
-//                     By {source.name} | updated at: {new Date(publishedAt).toDateString()} {publishedAt.substring(11, 19)}
-//                 </p>
-//             )}
-//             <p className="article-description">{description}</p>
-//             <p>{content}  <a href={url} target="_blank" rel="noopener noreferrer">
-//                 Read More
-//             </a></p>
-// 
-//         </div>
-//     );
-// };
-
 const NewsArticle = ({ article }) => {
     if (!article) {
-        // If article data is missing or undefined, display a modern fallback UI
         return (
             // <div className="fallback-ui">
             //   <img src="/placeholder-image.png" alt="Placeholder" className="fallback-image" />
@@ -54,9 +23,7 @@ const NewsArticle = ({ article }) => {
 
     return (
         <div className="news-article">
-            {image && (
-                <img src={image} alt={title} className="article-image" />
-            )}
+            
 
             <h2 className="article-title">{title}</h2>
             {author && <p className="article-author">By {author}</p>}
@@ -69,6 +36,9 @@ const NewsArticle = ({ article }) => {
             <p>{content}  <a href={url} target="_blank" rel="noopener noreferrer">
                 Read More
             </a></p>
+            {image && (
+                <img src={image} alt={title} className="article-image" />
+            )}
 
         </div>
     );
